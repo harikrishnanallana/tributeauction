ebba699ed023555293e6aac081079028042bf2a0
khaangnguyeen <78076796+khengyun@users.noreply.github.com>
2023-07-08 09:56:24 +0700

    update simple database

--- src/java/DBconnect/DBconnector.java
+++ src/java/DBconnect/DBconnector.java
@@ -4,6 +4,9 @@
  */
 package DBconnect;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -24,17 +27,16 @@ public class DBconnector {
     private String password = "niran@123456";
     private String portNumber = "1433";
 
-
     public Connection conn;
 
     public DBconnector() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             String connectorURL = "jdbc:sqlserver://" + hostName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
-            if (instance == null || instance.trim().isEmpty()){
-             connectorURL = "jdbc:sqlserver://" + hostName + ":" + portNumber +";databaseName=" + dbName;
-             }
-            
+            if (instance == null || instance.trim().isEmpty()) {
+                connectorURL = "jdbc:sqlserver://" + hostName + ":" + portNumber + ";databaseName=" + dbName;
+            }
+
             conn = DriverManager.getConnection(connectorURL, username, password);
         } catch (Exception e) {
             e.printStackTrace();
@@ -42,17 +44,57 @@ public class DBconnector {
     }
 
     public void close() {
-    try {
-        if (conn != null) {
-            conn.close();
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBconnector.class.getName()).log(Level.SEVERE, null, ex);
         }
-    } catch (SQLException ex) {
-        Logger.getLogger(DBconnector.class.getName()).log(Level.SEVERE, null, ex);
     }
-}
-    
-  
 
-    
-}
+    public static void main(String[] args) {
+        DBconnector dbConnector = new DBconnector();
 
+        // Kiểm tra kết nối thành công
+        if (dbConnector.conn != null) {
+            System.out.println("Kết nối thành công!");
+
+            try {
+                // Đường dẫn đến tệp .bat nằm ở thư mục bên ngoài
+                String batFilePath = System.getProperty("user.dir") + "\\src\\java\\DBconnect\\initialize.bat";
+
+
+                // Thực thi lệnh để chạy tệp .bat
+                ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", batFilePath);
+                processBuilder.redirectErrorStream(true); // Để hợp nhất stdout và stderr
+                Process process = processBuilder.start();
+
+                // Đọc dữ liệu đầu ra từ quá trình
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+                // Đợi quá trình hoàn thành
+                int exitCode = process.waitFor();
+
+                if (exitCode == 0) {
+                    System.out.println("Cơ sở dữ liệu đã được khởi tạo hoặc cập nhật thành công.");
+                } else {
+                    System.out.println("Khởi tạo cơ sở dữ liệu thất bại. Mã thoát: " + exitCode);
+                }
+
+                reader.close();
+            } catch (IOException | InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            System.out.println("Kết nối thất bại!");
+        }
+
+        dbConnector.close();
+    }
+
+}
---
+++ src/java/DBconnect/database.sql
@@ -0,0 +1,104 @@
+﻿USE master;
+GO
+
+-- Kill active sessions using AuctionDB database
+DECLARE @kill_sessions VARCHAR(MAX) = '';
+
+SELECT @kill_sessions = @kill_sessions + 'KILL ' + CAST(session_id AS VARCHAR) + ';' 
+FROM sys.dm_exec_sessions
+WHERE DB_Name(database_id) = 'AuctionDB';
+
+EXEC(@kill_sessions);
+GO
+
+-- Drop the database if it already exists
+IF DB_ID('AuctionDB') IS NOT NULL
+BEGIN
+    ALTER DATABASE AuctionDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE AuctionDB;
+END
+GO
+
+-- Create the database
+CREATE DATABASE AuctionDB;
+GO
+
+-- Switch to the AuctionDB database
+USE AuctionDB;
+GO
+
+-- Create the Users table
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY,
+    Username VARCHAR(50),
+    Password VARCHAR(50),
+    Email VARCHAR(50),
+    Role VARCHAR(10)
+);
+GO
+
+-- Insert sample data into the Users table
+INSERT INTO Users (UserID, Username, Password, Email, Role)
+VALUES
+    (1, 'client1', 'password1', 'client1@example.com', 'client1'),
+    (2, 'client2', 'password2', 'client2@example.com', 'client2'),
+    (3, 'admin', 'admin123', 'admin@example.com', 'admin');
+GO
+
+-- Create the Auctions table
+CREATE TABLE Auctions (
+    AuctionID INT PRIMARY KEY,
+    Title VARCHAR(100),
+    Description VARCHAR(200),
+    StartDate DATETIME,
+    EndDate DATETIME,
+    Status VARCHAR(20)
+);
+GO
+
+-- Insert sample data into the Auctions table
+INSERT INTO Auctions (AuctionID, Title, Description, StartDate, EndDate, Status)
+VALUES
+    (1, 'Auction 1', 'Description for Auction 1', '2023-07-08 12:00:00', '2023-07-10 12:00:00', 'Ongoing'),
+    (2, 'Auction 2', 'Description for Auction 2', '2023-07-09 12:00:00', '2023-07-11 12:00:00', 'Upcoming'),
+    (3, 'Auction 3', 'Description for Auction 3', '2023-07-06 12:00:00', '2023-07-07 12:00:00', 'Completed');
+GO
+
+-- Create the Bids table
+CREATE TABLE Bids (
+    BidID INT PRIMARY KEY,
+    AuctionID INT,
+    UserID INT,
+    Amount DECIMAL(10, 2),
+    BidTime DATETIME,
+    FOREIGN KEY (AuctionID) REFERENCES Auctions (AuctionID),
+    FOREIGN KEY (UserID) REFERENCES Users (UserID)
+);
+GO
+
+-- Insert sample data into the Bids table
+INSERT INTO Bids (BidID, AuctionID, UserID, Amount, BidTime)
+VALUES
+    (1, 1, 1, 100.00, '2023-07-08 12:05:00'),
+    (2, 1, 2, 120.00, '2023-07-08 12:07:00'),
+    (3, 2, 1, 200.00, '2023-07-09 12:10:00');
+GO
+
+-- Create the Items table
+CREATE TABLE Items (
+    ItemID INT PRIMARY KEY,
+    AuctionID INT,
+    ItemName VARCHAR(100),
+    Description VARCHAR(200),
+    CurrentBid DECIMAL(10, 2),
+    FOREIGN KEY (AuctionID) REFERENCES Auctions (AuctionID)
+);
+GO
+
+-- Insert sample data into the Items table
+INSERT INTO Items (ItemID, AuctionID, ItemName, Description, CurrentBid)
+VALUES
+    (1, 1, 'Item 1', 'Description for Item 1', 120.00),
+    (2, 2, 'Item 2', 'Description for Item 2', 200.00),
+    (3, 3, 'Item 3', 'Description for Item 3', 150.00);
+GO
