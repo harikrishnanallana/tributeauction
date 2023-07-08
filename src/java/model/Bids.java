b7b8ad49b46b90fbe3d7b2ae434096fac5c71f84
khaangnguyeen <78076796+khengyun@users.noreply.github.com>
2023-07-08 19:32:58 +0700

    update

--- src/java/model/Bids.java
+++ src/java/model/Bids.java
@@ -17,4 +17,57 @@ public class Bids {
     public int UserID;
     public LocalDateTime  BidTime;
     public BigDecimal Amount;
+
+    @Override
+    public String toString() {
+        return "Bids{" + "BidID=" + BidID + ", AuctionID=" + AuctionID + ", UserID=" + UserID + ", BidTime=" + BidTime + ", Amount=" + Amount + '}';
+    }
+
+    public int getBidID() {
+        return BidID;
+    }
+
+    public void setBidID(int BidID) {
+        this.BidID = BidID;
+    }
+
+    public int getAuctionID() {
+        return AuctionID;
+    }
+
+    public void setAuctionID(int AuctionID) {
+        this.AuctionID = AuctionID;
+    }
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(int UserID) {
+        this.UserID = UserID;
+    }
+
+    public LocalDateTime getBidTime() {
+        return BidTime;
+    }
+
+    public void setBidTime(LocalDateTime BidTime) {
+        this.BidTime = BidTime;
+    }
+
+    public BigDecimal getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(BigDecimal Amount) {
+        this.Amount = Amount;
+    }
+
+    public Bids(int BidID, int AuctionID, int UserID, LocalDateTime BidTime, BigDecimal Amount) {
+        this.BidID = BidID;
+        this.AuctionID = AuctionID;
+        this.UserID = UserID;
+        this.BidTime = BidTime;
+        this.Amount = Amount;
+    }
 }
